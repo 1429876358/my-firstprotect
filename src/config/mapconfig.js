ff3226248f352b1369e66d6e5147219aa9285c2d
1429876358 <1429876358@qq.com>
2020-03-30 12:13:44 +0800

    second

--- src/config/mapconfig.js
+++ src/config/mapconfig.js
@@ -3,7 +3,7 @@ import TileArcGISRest from 'ol/source/TileArcGISRest'
 import OSM from "ol/source/OSM"
 import XYZ from 'ol/source/XYZ'
 
-let maptype=2          //0表示部署的离线瓦片地图，1表示OSM,2表示使用Arcgis在线午夜蓝地图服务
+let maptype=1         //0表示部署的离线瓦片地图，1表示OSM,2表示使用Arcgis在线午夜蓝地图服务
 
 var streetmap=function(){
     var maplayer=null;
@@ -32,6 +32,7 @@ var streetmap=function(){
 }
 
 var mapconfig={
+
     x:114.064839,     //中心点经度和纬度
     y:22.548857,
     zoom:15,          //地图缩放级别
